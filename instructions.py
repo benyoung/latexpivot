a409fbd0f7ef90d8700142b2ca51a2934d3f47cd
Benjamin Young <speedymollusc@gmail.com>
2012-01-24 21:57:27 -0800

    major format change.  might not work yet

--- instructions.py
+++ instructions.py
@@ -1,22 +1,30 @@
-instructions = {
-    "keys": ("n", "s", "a", "b"),
-    #"change_variables": {
-    #    "t": "n-a"
-    #},
-    "rows": ("n"),
-    "columns": ("b"),
-    "pages": ("a","s"),
+# Change this to alter the way keys and values get assigned
+# rows, columns, and so forth.  
+class table:
+    row_heading = "$n = %d"
+    col_heading = "$e_{%d}"
+    page_heading = "Coefficient of $x^{%d}$ when $s=%d$"
 
-    "row headings":  "$n=%d$",
-    "col headings":  "$e_{%d}$",
-    "page headings": "Coefficient of $x^{%d}$ when $s=%d$",
-    #"filters": [
-    #    "s == a",
-    #]
-}
+    max_col = min(len(sortedcols), 15)
+    max_row = min(len(sortedrows), 1000)
+    max_page = min(len(sortedpages), 1000)
 
-def entry_to_string(record):
-    output = ""
-    for (a,b) in record:
-        output += "\\binom{%d}{%d}" % (a,b)
-    return output
+class datapoint:
+    def __init__(self, key, value):
+        (n, s, a, b) = key
+        self.row = (n)
+        self.column = (b)
+        self.page = (s,a)
+
+        self.show = true     # Change this to hide some entries
+
+        if self.show:
+            self.value = value
+
+    def render(self):
+        output = ""
+        for (a,b) in self.value:
+            output.append("$\\binom{%d}{%d}$" % (a,b))
+        return output
+
+
